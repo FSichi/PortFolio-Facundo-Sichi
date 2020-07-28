231cfeeba1f9ff0eea1ed8b4184c253b845996e0
Facundo Sichi <facusichi99@hotmail.com>
2020-07-28 19:56:54 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let ubicacionPrincipal = window.pageYOffset; //0
+
+  AOS.init();
+
+window.addEventListener("scroll", function(){
+    let desplazamientoActual = window.pageYOffset; //180
+    if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal= desplazamientoActual; //200
+
+})
+
+// Menu
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+let ismenu = false
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    if(semaforo){
+        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
+        semaforo= false;
+        ismenu = true;
+    }else{
+        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        semaforo= true;
+    }
+
+    enlacesHeader.classList.toggle("menudos")
+
+})
+
+
+document.querySelectorAll(".enlaces-header")[0].addEventListener("click", function(){
+
+    if(ismenu)
+    {
+        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        enlacesHeader.classList.toggle("menudos")
+    }
+
+})
